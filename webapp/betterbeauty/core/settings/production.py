0643408ebd7ca5cf05e5917c5cdf6030ba356e20
Alexei M <alexeidjango@gmail.com>
2018-04-12 20:29:53 +0000

    [HOTFIX] Fix s3 storage backend and bucket name (to use EB's instance dedicated bucket).

--- webapp/betterbeauty/core/settings/production.py
+++ webapp/betterbeauty/core/settings/production.py
@@ -4,5 +4,6 @@ ALLOWED_HOSTS = ('*.admin.betterbeauty.io', 'admin.betterbeauty.io', )
 
 DEBUG = False
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'betterbeauty-webapp-production-public'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = '<set when instance is ready>'
+AWS_LOCATION = 'uploads'
--- webapp/betterbeauty/core/settings/staging.py
+++ webapp/betterbeauty/core/settings/staging.py
@@ -4,5 +4,6 @@ ALLOWED_HOSTS = ('*.admindev.betterbeauty.io', 'admindev.betterbeauty.io', )
 
 DEBUG = True
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'betterbeauty-webapp-staging-public'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-east-1-024990310245'
+AWS_LOCATION = 'uploads'
