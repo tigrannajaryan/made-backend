6b74df60dbc042e9ab6040e0a962ce8018df910b
Tigran Najaryan <tigran@betterbeauty.io>
2018-04-24 03:07:46 +0000

    Fix linting errors
    Fixed 2 small linting errors.

--- mobile/src/providers/auth-service/auth-service.ts
+++ mobile/src/providers/auth-service/auth-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 import { BaseServiceProvider } from '../base-service';
 
