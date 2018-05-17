2a19dfe2fa66c57d8023571ed88b3fa929ab611c
dmitriy <di3orlive@yahoo.com>
2018-05-17 08:31:30 -0400

    Small discounts style fix (#64)

--- mobile/stylist/src/environments/environment.dev.ts
+++ mobile/stylist/src/environments/environment.dev.ts
@@ -1,7 +1,6 @@
 export const ENV = {
-  // apiUrl: 'http://betterbeauty.local:8000/api/v1/',
-  apiUrl: 'http://admindev.betterbeauty.io/api/v1/',
-  // apiUrl: 'http://192.168.31.109:8000/api/v1/',
+  apiUrl: 'http://betterbeauty.local:8000/api/v1/',
+  // apiUrl: 'http://admindev.betterbeauty.io/api/v1/',
   production: false,
   version: '_DEV_'
 };
