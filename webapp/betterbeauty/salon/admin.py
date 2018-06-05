58b4f3bdcb675471d65699916709722a41acdaa5
Alexei Mironov <alexeidjango@gmail.com>
2018-06-05 18:48:42 +0300

    Improve Stylist / Stylist Service admin section
    - Make StylistService.deleted_at read-only (hence not required)
    - Add custom validation for available days start/end (now they will only
      be required if `is_available` flag is set
    - Reorganize Stylist's admin page to group similar fields
    - Display service uuid to allow copy-pasting it to AppointmentService.
    
    Github issue: #91

--- webapp/betterbeauty/salon/admin.py
+++ webapp/betterbeauty/salon/admin.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django import forms
 from django.contrib import admin
 
 from .models import (
@@ -7,7 +10,6 @@ from .models import (
     ServiceTemplateSet,
     Stylist,
     StylistAvailableWeekDay,
-    StylistDateRangeDiscount,
     StylistService,
     StylistServicePhotoSample,
     StylistWeekdayDiscount,
@@ -30,15 +32,45 @@ class SalonAdmin(admin.ModelAdmin):
     pass
 
 
-class StylistAdmin(admin.ModelAdmin):
+class StylistAvailableDayForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(StylistAvailableDayForm, self).__init__(*args, **kwargs)
+        self.fields['work_start_at'].required = False
+        self.fields['work_end_at'].required = False
 
-    class StylistDateRangeDiscountInline(admin.StackedInline):
-        model = StylistDateRangeDiscount
-        extra = 0
+    class Meta:
+        model = StylistAvailableWeekDay
+        fields = ['weekday', 'work_start_at', 'work_end_at', 'is_available', ]
+
+    def clean(self):
+        cleaned_data = super(StylistAvailableDayForm, self).clean()
+        is_available: bool = cleaned_data.get('is_available')
+        form_errors = {}
+        if is_available:
+            work_start_at: datetime.time = cleaned_data.get('work_start_at', None)
+            work_end_at: datetime.time = cleaned_data.get('work_end_at', None)
+            if not work_start_at:
+                form_errors.update(
+                    {'work_start_at': 'Day is set to available, so start time is required'}
+                )
+            if not work_end_at:
+                form_errors.update(
+                    {'work_end_at': 'Day is set to available, so end time is required'})
+            if work_start_at and work_end_at and work_start_at > work_end_at:
+                form_errors.update(
+                    {'work_end_at': 'End time cannot be before start time'}
+                )
+        if form_errors:
+            raise forms.ValidationError(form_errors)
+        return cleaned_data
+
+
+class StylistAdmin(admin.ModelAdmin):
 
     class StylistAvailableDayInline(admin.TabularInline):
         model = StylistAvailableWeekDay
         extra = 1
+        form = StylistAvailableDayForm
 
     class StylistWeekdayDiscount(admin.StackedInline):
         model = StylistWeekdayDiscount
@@ -47,14 +79,24 @@ class StylistAdmin(admin.ModelAdmin):
     class StylistServiceInline(admin.StackedInline):
         model = StylistService
         extra = 0
+        readonly_fields = ['deleted_at', 'service_uuid', ]
         # TODO: add custom form here, to add photo samples
 
     class StylistServicePhotosampleInline(admin.TabularInline):
         model = StylistServicePhotoSample
         extra = 0
 
+    fieldsets = (
+        (None, {'fields': ('user', 'salon', )}),
+        ('Discounts', {'fields': ('first_time_book_discount_percent',
+                                  'rebook_within_1_week_discount_percent',
+                                  'rebook_within_2_weeks_discount_percent',
+                                  'is_discount_configured')
+                       }
+         ),
+    )
+
     inlines = [
-        StylistDateRangeDiscountInline,
         StylistWeekdayDiscount,
         StylistAvailableDayInline,
         StylistServiceInline,
