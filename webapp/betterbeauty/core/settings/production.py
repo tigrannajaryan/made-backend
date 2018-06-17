cfba963f220a627818ed0e47f0fa8958589a4b2d
Alexei Mironov <alexeidjango@gmail.com>
2018-06-18 00:55:30 +0300

    Update django settings for production deploy

--- webapp/betterbeauty/core/settings/production.py
+++ webapp/betterbeauty/core/settings/production.py
@@ -2,14 +2,13 @@ from .defaults import *  # noqa
 
 LEVEL = 'production'
 
-ALLOWED_HOSTS = ('*.admin.betterbeauty.io', 'admin.betterbeauty.io', )
+ALLOWED_HOSTS = ('*.admin.madebeauty.com', 'admin.madebeauty.com', )
 
 DEBUG = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = '<set when instance is ready>'
-AWS_LOCATION = 'uploads'
+AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-east-1-024990310245'
+AWS_LOCATION = 'uploads-production'
 
-# TODO: add FB settings
-FB_APP_ID = ''
-FB_APP_SECRET = ''
+FB_APP_ID = '1332946860139961'
+FB_APP_SECRET = 'a2013eb7dc891c5534bcad9ee69bae98'
--- webapp/betterbeauty/core/settings/staging.py
+++ webapp/betterbeauty/core/settings/staging.py
@@ -13,8 +13,7 @@ DEBUG = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-east-1-024990310245'
-AWS_LOCATION = 'uploads'
+AWS_LOCATION = 'uploads-staging'
 
-# TODO: add FB settings
 FB_APP_ID = '1332946860139961'
 FB_APP_SECRET = 'a2013eb7dc891c5534bcad9ee69bae98'
