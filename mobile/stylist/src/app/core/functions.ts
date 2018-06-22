b12812ba27102999feb6378a4a8db5f7120b7b4b
Tigran Najaryan <tigran@najaryan.net>
2018-06-22 13:28:48 -0400

    Fix failing build on Windows
    For some strange reason this is only failing on Windows but not on Linux.

--- mobile/stylist/src/app/core/functions.ts
+++ mobile/stylist/src/app/core/functions.ts
@@ -55,6 +55,8 @@ export function createNavHistoryList(profileStatus: ProfileStatus): PageDescr[] 
   return [{ page: PageNames.Today }];
 }
 
+declare const process: any; // make process variable visible to TypeScript
+
 export function getBuildNumber(): string {
   return (process.env.IOS_BUILD_NUMBER  || '0').trim();
 }
