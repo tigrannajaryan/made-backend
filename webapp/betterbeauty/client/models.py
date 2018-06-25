b0d1e5901c63d2b82bb7ae891a5dba9f219e3841
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2018-06-25 17:14:34 +0530

    FIX client get_full_name bug
    Removed client.user.get_full_name()

--- webapp/betterbeauty/client/models.py
+++ webapp/betterbeauty/client/models.py
@@ -16,6 +16,9 @@ class ClientOfStylist(models.Model):
         db_table = 'client_of_stylist'
         unique_together = (("stylist", "phone"), ("stylist", "first_name", "last_name"))
 
-    def __str__(self):
+    def get_full_name(self):
         full_name = '{0} {1}'.format(self.first_name, self.last_name)
         return full_name.strip()
+
+    def __str__(self):
+        return self.get_full_name()
