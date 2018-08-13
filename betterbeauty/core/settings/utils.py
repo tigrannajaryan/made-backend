d65d4896c00932dafc54f8c4f6bc14e8f677c707
Alexei <alexeidjango@users.noreply.github.com>
2018-08-13 14:33:20 +0300

    Setup loggly on AWS instances (#95)
    - add rsyslog configuration
    - add django logging config
    - fix revealed issue with cron

--- betterbeauty/core/settings/utils.py
+++ betterbeauty/core/settings/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import dj_database_url
 
 
@@ -15,3 +17,28 @@ def parse_database_url(database_url, ssl_cert=None):
     return dict(dj_database_url.parse(database_url),
                 TEST={'CHARSET': 'UTF8'},
                 **options)
+
+
+def get_handler_dict(local_path: str, filename: str, formatter: str) -> dict:
+    level = os.environ.get('LEVEL', '')
+    if level in ['staging', 'production']:
+        log_path = '/var/log/madebeauty'
+    else:
+        log_path = local_path
+    log_file_name = '{0}/{1}.log'.format(
+        log_path, filename
+    )
+    return {
+        'filename': log_file_name,
+        'formatter': formatter,
+        'level': 'DEBUG',
+        'class': 'logging.FileHandler'
+    }
+
+
+def get_logger_dict(handlers, level='INFO'):
+    return {
+        'handlers': handlers,
+        'level': level,
+        'propagate': True,
+    }
