e2fdc6bdc0fb3e5248e17c4a86b9c5bc844672a3
Aswin Kumar K P <kp.aswinkumar@gmail.com>
2018-09-10 09:27:10 +0530

    Allow geo-coding outside US (#192)
    - Change length of the state, zipcode
     - Set address to validate minimum 10 characters
    
    Github issue: #191

--- betterbeauty/integrations/gmaps.py
+++ betterbeauty/integrations/gmaps.py
@@ -5,6 +5,8 @@ import googlemaps
 from django.conf import settings
 from django.contrib.gis.geos import Point
 
+from api.v1.stylist.constants import MIN_VALID_ADDR_LEN
+
 
 class GeoCodedAddress(NamedTuple):
     city: Optional[str]
@@ -18,29 +20,40 @@ class GeoCodedAddress(NamedTuple):
 def geo_code(full_address: str):
     if not settings.IS_GEOCODING_ENABLED:
         return None
+    if len(full_address) < MIN_VALID_ADDR_LEN:
+        return None
     gmaps = googlemaps.Client(key=settings.GOOGLE_GEOCODING_API_KEY)
     geocode_results = gmaps.geocode(
-        address=full_address, region='us', components={'country': 'US'})
+        address=full_address)
     if len(geocode_results) != 1:
         return None
     geocode_result = geocode_results[0]
     if 'country' in geocode_result['types'] or (
             'partial_match' in geocode_result and geocode_result['partial_match']):
-        # Since we are filtering by US, if address is not found, US itself comes as the result.
-        # So returning None if that happens.
         return None
     address_components = geocode_result['address_components']
+    # address component is a list of dicts with type containing array of types.
+    locality = None
+    sublocality_level_1 = None
+    country = None
+    zip_code = None
+    state = None
+    # We iterate the address components to find the specific items that matches required type.
+    for item in address_components:
+        if 'locality' in item["types"]:
+            locality = item['short_name']
+        if 'sublocality_level_1' in item["types"]:
+            sublocality_level_1 = item['short_name']
+        if 'country' in item["types"]:
+            country = item['short_name']
+        if 'postal_code' in item["types"]:
+            zip_code = item['short_name']
+        if 'administrative_area_level_1' in item['types']:
+            state = item['short_name']
     # city is either returned in 'locality' or 'sublocality_level_1'
-    city = next((item['short_name'] for item in address_components
-                 if 'locality' in item["types"]),
-                next((item['short_name']
-                      for item in address_components
-                      if 'sublocality_level_1' in item["types"]), None))
-    state = next((item['short_name']
-                  for item in address_components
-                  if 'administrative_area_level_1' in item["types"]), None)
-    zip_code = next((item['short_name']
-                     for item in address_components if 'postal_code' in item["types"]), None)
+    city = locality if locality else sublocality_level_1
+    if not (country and city):
+        return None
     lat = geocode_result['geometry']['location']['lat']
     lng = geocode_result['geometry']['location']['lng']
 
