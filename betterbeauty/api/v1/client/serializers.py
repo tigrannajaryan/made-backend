ca4dc2b41c995d8ed08edeffbd2a53808dc7409b
Aswin Kumar K P <kp.aswinkumar@gmail.com>
2018-09-14 20:18:15 +0530

    Ignore uniqueness for email when not null (#211)
    Github issue: fix  #826

--- betterbeauty/api/v1/client/serializers.py
+++ betterbeauty/api/v1/client/serializers.py
@@ -62,7 +62,7 @@ class ClientProfileSerializer(FormattedErrorMessageMixin, serializers.ModelSeria
                   'profile_photo_url', 'zip_code', 'birthday', 'email', 'city', 'state']
 
     def validate_email(self, email: str):
-        if self.instance and Client.objects.exclude(
+        if email and self.instance and Client.objects.exclude(
                 pk=self.instance.client.pk).filter(email=email):
             raise serializers.ValidationError(ErrorMessages.ERR_UNIQUE_CLIENT_EMAIL)
         return email
