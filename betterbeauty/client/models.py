eafd3af1d294e91a40dbcf5259b091a87ae60582
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2018-09-15 20:06:32 +0530

    Fix validating length for geo-code bug
    Github issue: https://github.com/madebeauty/frontend/issues/724
    JIRA: FRON-724

--- betterbeauty/client/models.py
+++ betterbeauty/client/models.py
@@ -11,7 +11,7 @@ from django.utils.translation import gettext_lazy as _
 
 from appointment.utils import get_appointments_in_datetime_range
 from core.models import User
-from integrations.gmaps import geo_code
+from integrations.gmaps import GeoCode
 from utils.models import SmartModel
 
 logger = logging.getLogger(__name__)
@@ -30,7 +30,7 @@ class Client(models.Model):
     last_geo_coded = models.DateTimeField(blank=True, null=True, default=None)
 
     def geo_code_address(self):
-        geo_coded_address = geo_code(self.zip_code)
+        geo_coded_address = GeoCode(self.zip_code).geo_code()
         if geo_coded_address:
             self.city = geo_coded_address.city
             self.state = geo_coded_address.state
