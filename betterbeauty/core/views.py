c1d7f0a4f4b12d9ad213bb584e379f4e0d964e78
Alexei Mironov <alexeidjango@gmail.com>
2018-09-28 00:26:56 +0300

    Verify S3 write access during regular healthchecks
    On production, once every ~20 checks try to actually write to the
    bucket designated for user uploads; this allows to ensure we have
    necessary permissions to write to the bucket, and S3 connection
    is actually present.

--- betterbeauty/core/views.py
+++ betterbeauty/core/views.py
@@ -1,5 +1,9 @@
 import logging
 import os
+import random
+
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 
 from django.conf import settings
 from django.core.files.storage import default_storage
@@ -15,10 +19,37 @@ logger = logging.getLogger(__name__)
 class HealthCheckView(views.APIView):
     permission_classes = [permissions.AllowAny, ]
 
+    @staticmethod
+    def _should_verify_s3_write_access():
+        # We should only perform this check on production, and we'll
+        # try to make it roughly once every 20 health-checks to save on
+        # the S3 PUT operation costs
+        if settings.LEVEL != 'production':
+            return False
+        return random.random() < 1 / settings.AWS_S3_WRITE_ACCESS_TEST_PERIODICITY
+
+    @staticmethod
+    def _has_s3_write_access():
+        """Verifies if we can actually write to s3"""
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(name=settings.AWS_STORAGE_BUCKET_NAME)
+        try:
+            # Instance normally should not have access to delete files,
+            # so we will just try to create or overwrite existing file
+            # without deleting it. It should be enough to verify the
+            # PutObject / PutObjectACL operations which are essential
+            bucket.put_object(
+                Body=b'test', Key=settings.AWS_S3_WRITE_ACCESS_TEST_FILE_NAME,
+                ACL='private')
+        except (BotoCoreError, ClientError):
+            logger.exception('Instance failed S3 write test')
+            return False
+        return True
+
     def get(self, request):
         is_healthy = True
         # verify if blacklisted environment vars is not available
-        blacklisted_envs = ENV_BLACKLIST.get(settings.LEVEL, None)
+        blacklisted_envs = ENV_BLACKLIST.get(settings.LEVEL, [])
         # we loop through the blacklisted vars for the current env and
         # if there is anything, we unset the healthy flag.
         for env in blacklisted_envs:
@@ -32,10 +63,12 @@ class HealthCheckView(views.APIView):
         except OperationalError:
             is_healthy = False
             logger.exception('Could not get DB connection while doing health check')
-        # verify if S3 is available
-
+        # verify if we can write to S3 bucket
         try:
             assert default_storage.connection is not None
+            if self._should_verify_s3_write_access():
+                if not self._has_s3_write_access():
+                    is_healthy = False
         except Exception as e:
             is_healthy = False
             logger.exception('Could not get S3 connection while doing health check')
