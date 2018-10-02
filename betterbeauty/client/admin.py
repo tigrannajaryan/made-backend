c2c5d2fec2ec3750fe40a3786a36ffcb7f698140
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2018-10-03 01:09:33 +0530

    Add preferred-stylist page in admin
    JIRA: BAC-256

--- betterbeauty/client/admin.py
+++ betterbeauty/client/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 from salon.models import Stylist
-from .models import Client, ClientOfStylist
+from .models import Client, ClientOfStylist, PreferredStylist
 
 
 class ClientOfStylistAdmin(admin.ModelAdmin):
+    list_display = ['stylist', 'client', 'created_at']
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "stylist":
             kwargs["queryset"] = Stylist.objects.order_by('user__email')
@@ -14,5 +16,10 @@ class ClientOfStylistAdmin(admin.ModelAdmin):
             ClientOfStylistAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
+class PreferredStylistAdmin(admin.ModelAdmin):
+    list_display = ['stylist', 'client']
+
+
 admin.site.register(Client)
 admin.site.register(ClientOfStylist, ClientOfStylistAdmin)
+admin.site.register(PreferredStylist, PreferredStylistAdmin)
