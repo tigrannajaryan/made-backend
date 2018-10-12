3ff0d2cffdaf99e4d7ba901b48c94bae4e5ecf3b
Alexei <alexeidjango@users.noreply.github.com>
2018-10-12 15:13:59 +0300

    Deprecate ClientOfStylist - teardown (#275)
    - remove ClientOfStylist table
    - remove temporary fields

--- betterbeauty/client/models.py
+++ betterbeauty/client/models.py
@@ -8,7 +8,6 @@ from django.contrib.gis.db.models import PointField
 from django.db import models
 from django.db.models import Q
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 from appointment.types import AppointmentStatus
 from appointment.utils import get_appointments_in_datetime_range
@@ -109,30 +108,6 @@ class Client(models.Model):
                 datetime_start_at__lt=next_midnight,
                 status=AppointmentStatus.CHECKED_OUT)),
         ).order_by('-datetime_start_at')
-
-
-class ClientOfStylist(models.Model):
-    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
-    stylist = models.ForeignKey(
-        'salon.Stylist', related_name='clients_of_stylist', on_delete=models.PROTECT)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True, null=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True, null=True)
-    phone = models.CharField(max_length=20, blank=True, null=True, default=None)
-    client = models.ForeignKey(
-        Client, on_delete=models.PROTECT, blank=True, null=True, related_name='client_of_stylists')
-    created_at = models.DateTimeField(_('date joined'), auto_now_add=True, null=True)
-
-    class Meta:
-        db_table = 'client_of_stylist'
-        unique_together = (("stylist", "phone"),
-                           ("stylist", "client"),)
-
-    def get_full_name(self):
-        full_name = '{0} {1}'.format(self.first_name, self.last_name)
-        return full_name.strip()
-
-    def __str__(self):
-        return self.get_full_name()
 
 
 class PreferredStylist(SmartModel):
