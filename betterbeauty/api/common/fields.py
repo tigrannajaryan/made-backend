9a6c112ce87a2dcc6aef7c42da93f5969b6e2347
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2018-10-26 21:54:04 +0530

    Remove non-ascii in phone numbers
    JIRA: BAC-305

--- betterbeauty/api/common/fields.py
+++ betterbeauty/api/common/fields.py
@@ -5,6 +5,10 @@ from rest_framework.exceptions import ValidationError
 from api.common.constants import ErrorMessages
 
 
+def remove_non_ascii(s):
+    return ''.join(filter(lambda a: a.isdigit() or a == '+', s))
+
+
 class PhoneNumberField(serializers.CharField):
     default_error_messages = {
         'invalid': ErrorMessages.INVALID_PHONE_NUMBER,
@@ -14,8 +18,8 @@ class PhoneNumberField(serializers.CharField):
         try:
             phonenumber_object = phonenumbers.parse(data, "US")
             if phonenumbers.is_possible_number(phonenumber_object):
-                phone_number_to_save = phonenumbers.format_number(
-                    phonenumber_object, phonenumbers.PhoneNumberFormat.E164)
+                phone_number_to_save = remove_non_ascii(phonenumbers.format_number(
+                    phonenumber_object, phonenumbers.PhoneNumberFormat.E164))
                 return super(PhoneNumberField, self).to_internal_value(phone_number_to_save)
         except phonenumbers.NumberParseException:
             pass
