eca34a8665aa70454e35b3a07e6118065b1edf11
Aswin Kumar K P <kp.aswinkumar@gmail.com>
2018-11-04 15:53:46 +0530

    Add user_uuid in auth APIs (#310)
    JIRA: BAC-314

--- betterbeauty/core/utils/auth.py
+++ betterbeauty/core/utils/auth.py
@@ -3,10 +3,11 @@ from rest_framework_jwt.settings import api_settings as jwt_api_settings
 from api.v1.auth.serializers import AuthTokenSerializer, ClientAuthTokenSerializer
 
 
-def jwt_response_payload_handler(token, user=None, request=None):
+def jwt_response_payload_handler(token, user=None, request=None, orig_iat=None):
     return AuthTokenSerializer({
         'token': token,
         'expires_in': jwt_api_settings.JWT_EXPIRATION_DELTA.total_seconds(),
+        'created_at': orig_iat,
         'role': user.role,
     }, context={'user': user}).data
 
@@ -14,6 +15,7 @@ def jwt_response_payload_handler(token, user=None, request=None):
 def client_jwt_response_payload_handler(token, user=None, orig_iat=None, request=None):
     return ClientAuthTokenSerializer({
         'token': token,
+        'expires_in': jwt_api_settings.JWT_EXPIRATION_DELTA.total_seconds(),
         'created_at': orig_iat,
         'role': user.role,
     }, context={'user': user}).data
