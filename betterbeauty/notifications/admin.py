823daf104bbcde335647b62eed0cb122c7269096
Alexei Mironov <alexeidjango@gmail.com>
2018-11-07 20:42:19 +0300

    Add channel field to Notification and allow empty data

--- betterbeauty/notifications/admin.py
+++ betterbeauty/notifications/admin.py
@@ -4,8 +4,11 @@ from .models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ['code', 'user', 'target', 'pending_to_send', 'sent_at', 'device_acked_at', ]
-    list_filter = ['code', 'target', ]
+    list_display = [
+        'code', 'channel', 'user', 'target', 'pending_to_send',
+        'sent_at', 'device_acked_at',
+    ]
+    list_filter = ['code', 'target', 'channel', ]
     readonly_fields = ['sent_at', 'device_acked_at', ]
 
     class Meta:
--- betterbeauty/notifications/models.py
+++ betterbeauty/notifications/models.py
@@ -9,6 +9,8 @@ from timezone_field import TimeZoneField
 from core.choices import CLIENT_OR_STYLIST_ROLE
 from core.models import User
 
+from .types import NOTIFICATION_CNANNEL_CHOICES, NotificationChannel
+
 
 def default_json_field_value():
     return {}
@@ -20,7 +22,7 @@ class Notification(models.Model):
     target = models.CharField(choices=CLIENT_OR_STYLIST_ROLE, max_length=16)
     code = models.CharField(max_length=64, verbose_name='Notification code')
     message = models.CharField(max_length=512)
-    data = JSONField(default=default_json_field_value)
+    data = JSONField(default=default_json_field_value, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     send_time_window_start = models.TimeField()
     send_time_window_end = models.TimeField()
@@ -29,6 +31,10 @@ class Notification(models.Model):
     sent_at = models.DateTimeField(null=True)
     discard_after = models.DateTimeField()
     device_acked_at = models.DateTimeField(null=True)
+    channel = models.CharField(
+        max_length=16, null=True, choices=NOTIFICATION_CNANNEL_CHOICES,
+        default=NotificationChannel.PUSH
+    )
 
     def __str__(self):
         return '{0} -> {1} {2}'.format(
