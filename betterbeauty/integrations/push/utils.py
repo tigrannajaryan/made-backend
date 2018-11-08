3641e7ed7654b6b7d3d93a6ba80a751cc74e0087
Alexei <alexeidjango@users.noreply.github.com>
2018-11-08 03:50:59 +0300

    Notifications - part 2
    - basic logic around sending notifications (conditions and constraints)
    - admin section improvements (now it will be possible to test notifications
      from djangoadmin)
    - badge count calculation
    - global send function to be used from the mgmt command.

--- betterbeauty/integrations/push/utils.py
+++ betterbeauty/integrations/push/utils.py
@@ -1,7 +1,8 @@
 import logging
-from typing import Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union
 
 from django.db import transaction
+from push_notifications import NotificationError
 from push_notifications.models import APNSDevice, GCMDevice
 
 from core.models import User
@@ -135,3 +136,61 @@ def unregister_device(
         )
     )
     return False
+
+
+def send_message_to_apns_devices_of_user(
+        user: User, user_role: UserRole, message: str,
+        extra: Dict[str, Any], badge_count: Optional[int]=0
+):
+    """
+        Bulk send message to all configured APNS installed apps matching target
+        :param user: User to whom devices belong
+        :param user_role: Type of application (client/stylist)
+        :param message: Text of the message
+        :param extra: Dictionary with arbitrary data to attach to payload
+        :param badge_count: positive int number to display on badge; 0 clears the badge
+        :return: True if operation was successful, False otherwise
+    """
+    eligible_app_ids = {
+        UserRole.STYLIST: [MobileAppIdType.IOS_STYLIST, MobileAppIdType.IOS_STYLIST_DEV],
+        UserRole.CLIENT: [MobileAppIdType.IOS_CLIENT, MobileAppIdType.IOS_CLIENT_DEV]
+    }[user_role]
+    apns_devices = user.apnsdevice_set.filter(
+        application_id__in=eligible_app_ids, active=True)
+    try:
+        apns_devices.send_message(message=message, extra=extra, badge=badge_count)
+    except NotificationError:
+        logger.exception('Failed APNS notification(s) to {0} user {1}'.format(
+            user_role, user.uuid
+        ))
+
+
+def send_message_to_fcm_devices_of_user(
+        user: User, user_role: UserRole, message: str,
+        extra: Dict[str, Any], badge_count: Optional[int]=0
+):
+    """
+    Bulk send message to all configured GCM/FCM installed apps matching target
+    :param user: User to whom devices belong
+    :param user_role: Type of application (client/stylist)
+    :param message: Text of the message
+    :param extra: Dictionary with arbitrary data to attach to payload
+    :param badge_count: positive int number to display on badge; 0 clears the badge
+    :return: True if operation was successful, False otherwise
+    """
+    eligible_app_ids = {
+        UserRole.STYLIST: [MobileAppIdType.ANDROID_STYLIST, ],
+        UserRole.CLIENT: [MobileAppIdType.ANDROID_CLIENT, ]
+    }[user_role]
+    # Based on django_push_notifications documentation, we should specifically add
+    # this filter (`cloud_message_type='FCM'`)
+    fcm_devices = user.gcmdevice_set.filter(
+        application_id__in=eligible_app_ids, active=True, cloud_message_type='FCM')
+    try:
+        fcm_devices.send_message(message=message, extra=extra, badge=badge_count)
+        return True
+    except NotificationError:
+        logger.exception('Failed FCM notification(s) to {0} user {1}'.format(
+            user_role, user.uuid
+        ))
+        return False
