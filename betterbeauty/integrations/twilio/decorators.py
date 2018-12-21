471367c82c8c7494b0049798b1f8fcb8da9182e3
Alexei <alexeidjango@users.noreply.github.com>
2018-12-21 18:46:46 +0300

    [HOTFIX] Temporary code to investigate issue with Twilio request authentication (#428)

--- betterbeauty/integrations/twilio/decorators.py
+++ betterbeauty/integrations/twilio/decorators.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from functools import wraps
 
@@ -5,21 +6,32 @@ from django.http import HttpResponseForbidden
 from twilio.request_validator import RequestValidator
 
 
+logger = logging.getLogger(__name__)
+
+
 def twilio_auth_required(view_func):
     """Decorator checks whether the call made by Twilio service. INH."""
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
+        # TODO: remove once after testing finishes
         twilio_signature = request.META.get('HTTP_X_TWILIO_SIGNATURE')
-
+        logger.info('TWILIO AUTH: got twilio_signature: %s' % twilio_signature)
         if not twilio_signature:
             return HttpResponseForbidden()
-
-        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN', None))
+        auth_token = os.environ.get('TWILIO_AUTH_TOKEN', None)
+        logger.info(
+            'TWILIO AUTH: got twilio token: %s' % auth_token[-5:]
+            if auth_token else 'None'
+        )
+        validator = RequestValidator(auth_token)
         url = request.build_absolute_uri()
+        logger.info('TWILIO AUTH: got url: %s' % url)
         payload = request.POST.dict()
+        logger.info('TWILIO AUTH: Payload: {0}'.format(payload))
         if not validator.validate(url, payload, twilio_signature):
+            logger.info('TWILIO AUTH: validation failed')
             return HttpResponseForbidden()
-
+        logger.info('TWILIO AUTH: validation passed')
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
