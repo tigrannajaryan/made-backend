fdeadd686b6fc4b2194269f88bb9f19ba084e3be
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2018-12-30 00:36:44 +0530

    Remind add photo notification
    JIRA: BAC-406

--- betterbeauty/notifications/tests/test_utils.py
+++ betterbeauty/notifications/tests/test_utils.py
@@ -38,6 +38,7 @@ from ..utils import (
     generate_hint_to_rebook_notifications,
     generate_hint_to_select_stylist_notifications,
     generate_new_appointment_notification,
+    generate_remind_add_photo_notifications,
     generate_remind_invite_clients_notifications,
     generate_stylist_registration_incomplete_notifications,
     generate_tomorrow_appointments_notifications,
@@ -90,18 +91,13 @@ def busy_stylist() -> Stylist:
 
 
 @pytest.fixture
-def stylist_eligible_for_invite_reminder() -> Stylist:
-    """Generate a stylist perfectly eligible for REMIND_INVITE_CLIENTS notification"""
+def stylist_eligible_for_notification(stylist_created_before_days=89) -> Stylist:
+    """Generate a generic stylist eligible for sending notification"""
+
     user: User = G(User, phone='123456')
     stylist: Stylist = G(
-        Stylist, created_at=timezone.now() - datetime.timedelta(days=89),
+        Stylist, created_at=timezone.now() - datetime.timedelta(days=stylist_created_before_days),
         has_business_hours_set=True, deactivated_at=None, user=user
-    )
-    # let there be the same notification sent > 30 days ago
-    G(
-        Notification, code=NotificationCode.REMIND_INVITE_CLIENTS,
-        created_at=timezone.now() - datetime.timedelta(days=31), target=UserRole.STYLIST,
-        user=user, pending_to_send=False
     )
     # another notification sent more that 24hr ago
     G(
@@ -111,6 +107,30 @@ def stylist_eligible_for_invite_reminder() -> Stylist:
     )
     G(StylistService, stylist=stylist, is_enabled=True)
     G(APNSDevice, user=stylist.user, application_id=MobileAppIdType.IOS_STYLIST_DEV)
+    return stylist
+
+
+@pytest.fixture
+def stylist_eligible_for_invite_reminder() -> Stylist:
+    """Generate a stylist perfectly eligible for REMIND_INVITE_CLIENTS notification"""
+    stylist = stylist_eligible_for_notification(stylist_created_before_days=89)
+    user = stylist.user
+    # let there be the same notification sent > 30 days ago
+    G(
+        Notification, code=NotificationCode.REMIND_INVITE_CLIENTS,
+        created_at=timezone.now() - datetime.timedelta(days=31), target=UserRole.STYLIST,
+        user=user, pending_to_send=False
+    )
+    return stylist
+
+
+@pytest.fixture
+def stylist_eligible_for_photo_reminder() -> Stylist:
+    """Generate a stylist perfectly eligible for REMIND_ADD_PHOTO notification"""
+    stylist = stylist_eligible_for_notification(stylist_created_before_days=10)
+    user = stylist.user
+    user.photo = ''
+    user.save()
     return stylist
 
 
@@ -934,3 +954,68 @@ class TestGenerateRemindInviteClientsNotifications(object):
     def test_stylist_with_invitations(self, stylist_eligible_for_invite_reminder: Stylist):
         G(Invitation, stylist=stylist_eligible_for_invite_reminder)
         assert (generate_remind_invite_clients_notifications() == 0)
+
+
+class TestGenerateRemindAddPhotoNotifications(object):
+    @pytest.mark.django_db
+    def test_positive_path(self, stylist_eligible_for_photo_reminder: Stylist):
+        assert(Notification.objects.count() == 1)
+        assert(generate_remind_add_photo_notifications() == 1)
+        assert (Notification.objects.count() == 2)
+        notification: Notification = Notification.objects.last()
+        assert(notification.code == NotificationCode.REMIND_ADD_PHOTO)
+        assert(notification.target == UserRole.STYLIST)
+        assert(notification.user == stylist_eligible_for_photo_reminder.user)
+        assert(notification.send_time_window_start == datetime.time(10, 0))
+        assert(notification.send_time_window_end == datetime.time(20, 0))
+
+    @pytest.mark.django_db
+    def test_with_pending_notifications(self, stylist_eligible_for_photo_reminder: Stylist):
+        G(
+            Notification, target=UserRole.STYLIST, user=stylist_eligible_for_photo_reminder.user,
+            code='whatever', pending_to_send=True, sent_at=None
+        )
+        assert (generate_remind_add_photo_notifications() == 0)
+
+    @pytest.mark.django_db
+    def test_recent_notifications(self, stylist_eligible_for_photo_reminder: Stylist):
+        G(
+            Notification, target=UserRole.STYLIST, user=stylist_eligible_for_photo_reminder.user,
+            code='whatever', pending_to_send=False, sent_at=timezone.now() - datetime.timedelta(
+                hours=23
+            )
+        )
+        assert (generate_remind_add_photo_notifications() == 0)
+
+    @pytest.mark.django_db
+    def test_with_add_photo_notifications(self, stylist_eligible_for_invite_reminder):
+        G(
+            Notification, target=UserRole.STYLIST, user=stylist_eligible_for_invite_reminder.user,
+            code=NotificationCode.REMIND_INVITE_CLIENTS,
+            pending_to_send=False, sent_at=timezone.now() - datetime.timedelta(days=29)
+        )
+        assert (generate_remind_invite_clients_notifications() == 0)
+
+    @pytest.mark.django_db
+    def test_partial_profile(self, stylist_eligible_for_photo_reminder: Stylist):
+        user = stylist_eligible_for_photo_reminder.user
+        user.phone = None
+        user.save()
+        assert(stylist_eligible_for_photo_reminder.is_profile_bookable is False)
+        assert (generate_remind_add_photo_notifications() == 0)
+
+    @pytest.mark.django_db
+    def test_non_recent_stylist(self, stylist_eligible_for_photo_reminder: Stylist):
+        stylist_eligible_for_photo_reminder.created_at = timezone.now() - datetime.timedelta(
+            days=32
+        )
+        stylist_eligible_for_photo_reminder.save()
+        assert (generate_remind_add_photo_notifications() == 0)
+
+    @pytest.mark.django_db
+    def test_stylist_with_photo(self, stylist_eligible_for_photo_reminder: Stylist):
+        user = stylist_eligible_for_photo_reminder.user
+        dummy_user = G(User)
+        user.photo = dummy_user.photo
+        user.save()
+        assert (generate_remind_add_photo_notifications() == 0)
